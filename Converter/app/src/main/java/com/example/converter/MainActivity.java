bf1b878a380562228513980dcca2d6e6107f1421
StYaroslav <yaroslavstenko@gmail.com>
2020-10-19 00:17:54 +0300

    added versioning

--- Converter/app/src/main/java/com/example/converter/MainActivity.java
+++ Converter/app/src/main/java/com/example/converter/MainActivity.java
@@ -10,24 +10,18 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private MainFragment fragment;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         TextView appVersion = this.findViewById(R.id.appVersion);
-        setAppVersion(appVersion);
+        appVersion.setText(BuildConfig.VERSION_CODE + " " + BuildConfig.VERSION_NAME);
         fragment = (MainFragment) this.getSupportFragmentManager().findFragmentById(R.id.fragmentMain);
     }
 
-    @SuppressLint("SetTextI18n")
-    public void setAppVersion(TextView text) {
-        if (BuildConfig.FLAVOR.equals("pro")) {
-            text.setText("Converter Pro" + " " +BuildConfig.VERSION_CODE + " " + BuildConfig.VERSION_NAME);
-        } else {
-            text.setText("Converter" + " " +BuildConfig.VERSION_CODE + " " + BuildConfig.VERSION_NAME);
-        }
-    }
+
 
     public void switchButtonOnClick(View view) {
         assert fragment != null;
